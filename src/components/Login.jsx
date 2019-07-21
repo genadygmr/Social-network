14849a0ae42d9c59f6de3fca49a9ad6c4c3f68f5
Genady <genadymgr@gmail.com>
2019-07-21 21:04:42 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,62 +1,78 @@
-import React from  'react';
+import React from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
-import { withRouter } from 'react-router-dom';
+import {Router,Route,withRouter} from 'react-router';
 
 
-export default class Login extends React.Component {
+class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {user: "", pass: ""};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { user: "", pass: "" };
+  }
 
-    handleChange = (e, {name, value}) => this.setState({[name] : value});
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
 
-    handleSubmit = () => {
-        /*
-        fetch to login - if login success then retrieve uesr id.
-        use the user id to navigate to the correct user page - and then inide the page
-        in the 'componentWillMount' - retrieve all the relevant data according to the 
-        user id
-        */
-        const userId = "15";
+  handleSubmit = async () => {
+    let urlSeachParams = new URLSearchParams();
+    urlSeachParams.set("username", this.state.user);
+    urlSeachParams.set("password", this.state.pass);
+    urlSeachParams.set("grant_type", "password");
 
-        this.props.history.push(`/user/${userId}`);
-    } 
-    
 
-    render() {
-        return (
-            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }}>
-              <Header as='h2' color='teal' textAlign='center'>
-                Log-in to your account
+    let res = await fetch("https://localhost:44318/token", {
+      headers: { "Content-type": "application/x-www-form-urlencoded" },
+      method: "POST",
+      body: urlSeachParams
+
+    });
+    if (res.ok) {
+      res = await res.json();
+      localStorage.setItem("token", res.access_token);
+
+      await this.goToPersonalPage();
+    } else { console.error("Could not get authorization token"); }
+  }
+
+  goToPersonalPage = async () => {
+    // load all user data for his page
+    // hadle here the moving to the personal pages after the login
+    this.props.history.push('/user/23');
+  }
+
+
+  render() {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'>
+            Log-in to your account
               </Header>
-              <Form size='large'>
-                <Segment stacked>
-                  <Form.Input fluid icon='user' iconPosition='left' name='user' placeholder='E-mail address' onChange={this.handleChange}/>
-                  <Form.Input
-                    fluid
-                    icon='lock'
-                    iconPosition='left'
-                    placeholder='Password'
-                    type='password'
-                    name='pass'
-                    onChange={this.handleChange}
-                  />
-        
-                  <Button color='teal' fluid size='large' onClick={this.handleSubmit}>
-                    Login
+          <Form size='large'>
+            <Segment stacked>
+              <Form.Input fluid icon='user' iconPosition='left' name='user' placeholder='E-mail address' onChange={this.handleChange} />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+                name='pass'
+                onChange={this.handleChange}
+              />
+
+              <Button color='teal' fluid size='large' onClick={this.handleSubmit}>
+                Login
                   </Button>
-                </Segment>
-              </Form>
-              <Message>
-                New user? <a href='#'>Sign Up</a>
-              </Message>
-            </Grid.Column>
-          </Grid>
-        )
-    } 
+            </Segment>
+          </Form>
+          <Message>
+            New user? <a href='/signup'>Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
+export default withRouter(Login);
